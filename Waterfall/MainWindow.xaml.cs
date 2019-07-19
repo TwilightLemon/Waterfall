e9aa5675f20e505ffbb48d4c5a6e071a4e6ad998
TwilightLemon <cz241126@live.com>
2019-07-19 12:08:38 +0800

    添加项目文件。

---
+++ Waterfall/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+
+namespace Waterfall
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// 屏保使用方法:编译后将文件后缀名改为.scr 邮件安装即可.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Timer t = new Timer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            t.Interval = 5000;
+            t.Tick += delegate {
+                cl.Text = DateTime.Now.ToString("hh");
+                min.Text = DateTime.Now.ToString("mm");
+                date.Text = DateTime.Now.Month + " / " + DateTime.Now.Day + "    " + DateTime.Now.DayOfWeek.ToString();
+            };
+            cl.Text = DateTime.Now.ToString("hh");
+            min.Text= DateTime.Now.ToString("mm");
+            date.Text = DateTime.Now.Month + " / " + DateTime.Now.Day + "    " + DateTime.Now.DayOfWeek.ToString();
+            Top = 0;
+            Left = 0;
+            Width = SystemParameters.PrimaryScreenWidth;
+            Height = SystemParameters.PrimaryScreenHeight;
+            string path = Environment.ExpandEnvironmentVariables(@"%AppData%\Microsoft\Windows\Themes\TranscodedWallpaper");
+            var dt = new Bitmap(path);
+            var rect = new Rectangle(0, 0, dt.Width, dt.Height);
+            dt.GaussianBlur(ref rect, 80);
+            window.Background = new ImageBrush(new BitmapImage(new Uri(path)));
+            bk.Background = new ImageBrush(dt.ToBitmapImage()) { Stretch = Stretch.UniformToFill };
+            Loaded += MainWindow_LoadedAsync;
+            t.Start();
+        }
+
+        private async void MainWindow_LoadedAsync(object sender, RoutedEventArgs e)
+        {
+            var a=await WeatherLib.GetWeatherAsync("临沧");
+            im.ImageBrush = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/images/" + a.code+".png")));
+            qw.Text = a.Qiwen + "  " + a.MiaoShu + "  " + a.Data[0].QiWen;
+            kq.Text = "空气质量:"+a.KongQiZhiLiang;
+            var da = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(1));
+            da.RepeatBehavior = RepeatBehavior.Forever;
+            da.AutoReverse = true;
+            pp.BeginAnimation(OpacityProperty, da);
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var dta = new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(300));
+            dta.Completed += async delegate {
+                await Task.Delay(500);
+                var value = 0 - SystemParameters.PrimaryScreenHeight;
+                var ti = TimeSpan.FromSeconds(0.8);
+                var da = new DoubleAnimationUsingKeyFrames();
+                var ed = new EasingDoubleKeyFrame(value, ti);
+                ed.EasingFunction = new QuinticEase() { EasingMode = EasingMode.EaseInOut };
+                da.KeyFrames.Add(ed);
+                da.Completed += async delegate {
+                    await Task.Delay(1000);
+                    System.Windows.Application.Current.Shutdown();
+                };
+                BeginAnimation(TopProperty, da);
+            };
+            bn.BeginAnimation(OpacityProperty, dta);
+        }
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            Window_MouseDown(null, null);
+        }
+    }
+}
---
+++ Waterfall/WeatherLib.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterfall
+{
+    public class InfoHelper {
+        public class Weather
+        {
+            public string Qiwen { get; set; }
+            public string code { get; set; }
+            public string KongQiZhiLiang { get; set; }
+            public string MiaoShu { get; set; }
+            public string TiGan { get; set; }
+            public string FenSu { get; set; }
+            public string NenJianDu { get; set; }
+            public List<WeatherByDay> Data { get; set; }
+
+        }
+        public class WeatherByDay
+        {
+            public string Date { set; get; }
+            public string code { set; get; }
+            public string QiWen { set; get; }
+        }
+    }
+    public class HttpHelper {
+        public static async Task<string> GetWebAsync(string url, Encoding e = null)
+        {
+            if (e == null)
+                e = Encoding.UTF8;
+            HttpWebRequest hwr = (HttpWebRequest)WebRequest.Create(url);
+            hwr.Timeout = 20000;
+            var o = await hwr.GetResponseAsync();
+            StreamReader sr = new StreamReader(o.GetResponseStream(), e);
+            var st = await sr.ReadToEndAsync();
+            sr.Dispose();
+            return st;
+        }
+    }
+    public class WeatherLib
+    {
+        public static async Task<InfoHelper.Weather> GetWeatherAsync(string i)
+        {
+            InfoHelper.Weather dt = new InfoHelper.Weather();
+            //空气质量
+            var oj= JObject.Parse(await HttpHelper.GetWebAsync($"https://free-api.heweather.net/s6/air/now?parameters&location={Uri.EscapeUriString(i)}&key=f97e6a6ad4cd49babd0538747c86b88d"));
+            dt.KongQiZhiLiang = oj["HeWeather6"][0]["air_now_city"]["qlty"]+"  "+ oj["HeWeather6"][0]["air_now_city"]["aqi"];
+            //实况天气
+            JObject obj = JObject.Parse(await HttpHelper.GetWebAsync($"https://free-api.heweather.net/s6/weather/now?parameters&location={Uri.EscapeUriString(i)}&key=f97e6a6ad4cd49babd0538747c86b88d"));
+            dt.TiGan = "体感温度:" + obj["HeWeather6"][0]["now"]["fl"] + "℃";
+            dt.FenSu= obj["HeWeather6"][0]["now"]["wind_dir"] + "    " + obj["HeWeather6"][0]["now"]["wind_sc"] + "级";
+            dt.MiaoShu = obj["HeWeather6"][0]["now"]["cond_txt"].ToString();
+            dt.code= obj["HeWeather6"][0]["now"]["cond_code"].ToString();
+            dt.NenJianDu= "能见度:" + obj["HeWeather6"][0]["now"]["vis"];
+            dt.Qiwen= obj["HeWeather6"][0]["now"]["tmp"] + "℃";
+            //未来几天的预报
+            JObject obj1 = JObject.Parse(await HttpHelper.GetWebAsync($"https://free-api.heweather.net/s6/weather/forecast?parameters&location={Uri.EscapeUriString(i)}&key=f97e6a6ad4cd49babd0538747c86b88d"));
+            List<InfoHelper.WeatherByDay> ddt = new List<InfoHelper.WeatherByDay>();
+            ddt.Add(new InfoHelper.WeatherByDay() {code=obj1["HeWeather6"][0]["daily_forecast"][0]["cond_code_d"].ToString(),
+            Date= obj1["HeWeather6"][0]["daily_forecast"][0]["date"].ToString(),
+            QiWen= obj1["HeWeather6"][0]["daily_forecast"][0]["tmp_max"] + "℃ -" + obj1["HeWeather6"][0]["daily_forecast"][0]["tmp_min"] + "℃"
+            });
+            for (int ix = 0; ix < obj1["HeWeather6"][0]["daily_forecast"].Count(); ix++) {
+                ddt.Add(new InfoHelper.WeatherByDay()
+                {
+                    code=obj1["HeWeather6"][0]["daily_forecast"][ix]["cond_code_d"].ToString(),
+                    Date = obj1["HeWeather6"][0]["daily_forecast"][ix]["date"].ToString(),
+                    QiWen = obj1["HeWeather6"][0]["daily_forecast"][ix]["tmp_max"] + "℃ -" + obj1["HeWeather6"][0]["daily_forecast"][ix]["tmp_min"] + "°C"
+                });
+            }
+            dt.Data = ddt;
+            return dt;
+        }
+    }
+}
